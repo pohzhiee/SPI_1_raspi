ff8f62f3ba4d699e27dbf1e825663cf13fb1a0ec
Poh Zhi-Ee <ZhiEe-Poh@ecosysgrp.com>
2021-04-01 11:11:48 +0800

    Commit1

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "pigpio.h"
+
+#define LOOPS 4
+#define SPEED 1000000
+#define BYTES 10
+int main(){
+   int loops=LOOPS;
+   int speed=SPEED;
+   int bytes=BYTES;
+   int i;
+   int h, h2;
+   double start, diff;
+   char buf[50] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   char bufRx[50];
+
+   if (gpioInitialise() < 0) return 1;
+
+   h = spiOpen(0, speed, 0);
+
+   if (h < 0) return 2;
+
+   start = time_time();
+
+   for (i=0; i<loops; i++)
+   {
+      spiXfer(h, buf, bufRx, bytes);
+      gpioWrite(15, i%2);
+   }
+
+   diff = time_time() - start;
+
+   printf("sps=%.1f: %d bytes @ %d bps (loops=%d time=%.1f)\n",
+      (double)loops / diff, bytes, speed, loops, diff);
+
+   spiClose(h);
+
+   gpioTerminate();
+
+}
